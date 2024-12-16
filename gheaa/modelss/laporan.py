da60db3a2cccd8d2ecfc44068e2071ba6bb423f3
Chandra Tritaqwa Ramadhan <lolpotch@gmail.com>
2024-12-16 15:03:48 +0700

    laporan & transaksi

--- gheaa/modelss/laporan.py
+++ gheaa/modelss/laporan.py
@@ -1,21 +1,43 @@
 from datetime import date
-from modelss.data import Data
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
 
-class Laporan(Data):
-    def __init__(self, id_laporan: int, alamat_kirim: str, deskripsi: str, tanggal: date):
-        self.id_laporan = id_laporan
+Base = declarative_base()
+
+class Laporan(Base):
+    __tablename__ = 'laporan'
+
+    id_laporan = Column(Integer, primary_key=True, autoincrement=True)
+    alamat_kirim = Column(String(255), nullable=False)
+    deskripsi = Column(String(500), nullable=False)
+    tanggal = Column(Date, default=date.today)
+
+    def __init__(self, alamat_kirim: str, deskripsi: str, tanggal: date = date.today()):
         self.alamat_kirim = alamat_kirim
         self.deskripsi = deskripsi
         self.tanggal = tanggal
 
-    def TambahData(self):
+    def TambahData(self, session):
+        session.add(self)
+        session.commit()
         print("Laporan ditambahkan")
 
-    def EditData(self):
+    def EditData(self, session, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        session.commit()
         print("Laporan diedit")
 
-    def HapusData(self):
+    def HapusData(self, session):
+        session.delete(self)
+        session.commit()
         print("Laporan dihapus")
 
-    def CariData(self):
-        print("Laporan dicari")
+    @staticmethod
+    def CariData(session, id_laporan):
+        laporan = session.query(Laporan).filter_by(id_laporan=id_laporan).first()
+        if laporan:
+            print(f"Laporan ditemukan: {laporan}")
+        else:
+            print("Laporan tidak ditemukan")
+        return laporan
--- gheaa/modelss/transaksi.py
+++ gheaa/modelss/transaksi.py
@@ -1,23 +1,47 @@
 from datetime import date
-from modelss.data import Data
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
 
-class Transaksi(Data):
-    def __init__(self, id_transaksi: int, tanggal: date, nama_pelanggan: str, jenis_barang: int, jumlah: int, stat_bayar: str):
-        self.id_transaksi = id_transaksi
+Base = declarative_base()
+
+class Transaksi(Base):
+    __tablename__ = 'transaksi'
+
+    id_transaksi = Column(Integer, primary_key=True, autoincrement=True)
+    tanggal = Column(Date, default=date.today)
+    nama_pelanggan = Column(String(255), nullable=False)
+    jenis_barang = Column(Integer, nullable=False)
+    jumlah = Column(Integer, nullable=False)
+    stat_bayar = Column(String(50), nullable=False)
+
+    def __init__(self, tanggal: date, nama_pelanggan: str, jenis_barang: int, jumlah: int, stat_bayar: str):
         self.tanggal = tanggal
         self.nama_pelanggan = nama_pelanggan
         self.jenis_barang = jenis_barang
         self.jumlah = jumlah
         self.stat_bayar = stat_bayar
 
-    def TambahData(self):
+    def TambahData(self, session):
+        session.add(self)
+        session.commit()
         print("Transaksi ditambahkan")
 
-    def EditData(self):
+    def EditData(self, session, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        session.commit()
         print("Transaksi diedit")
 
-    def HapusData(self):
+    def HapusData(self, session):
+        session.delete(self)
+        session.commit()
         print("Transaksi dihapus")
 
-    def CariData(self):
-        print("Transaksi dicari")
+    @staticmethod
+    def CariData(session, id_transaksi):
+        transaksi = session.query(Transaksi).filter_by(id_transaksi=id_transaksi).first()
+        if transaksi:
+            print(f"Transaksi ditemukan: {transaksi}")
+        else:
+            print("Transaksi tidak ditemukan")
+        return transaksi
