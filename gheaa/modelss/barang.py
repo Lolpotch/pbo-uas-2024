d4fae33bf19662d8e1b11f46c2f4b542227941ee
Chandra Tritaqwa Ramadhan <lolpotch@gmail.com>
2024-12-16 14:50:08 +0700

    Update barang.py

--- gheaa/modelss/barang.py
+++ gheaa/modelss/barang.py
@@ -2,13 +2,13 @@ from datetime import date
 from modelss.data import Data
 
 class Barang(Data):
-    def __init__(self, id_barang: int, jenis_barang: str, stok_gudang: int, barang_masuk: int, barang_keluar: int, Tanggal: date):
+    def __init__(self, id_barang: int, jenis_barang: str, stok_gudang: int, barang_masuk: int, barang_keluar: int, tanggal: date):
         self.id_barang = id_barang
         self.jenis_barang = jenis_barang
         self.stok_gudang = stok_gudang
         self.barang_masuk = barang_masuk
         self.barang_keluar = barang_keluar
-        self.Tanggal = Tanggal
+        self.tanggal = tanggal
 
     def TambahData(self):
         print("Barang ditambahkan")
