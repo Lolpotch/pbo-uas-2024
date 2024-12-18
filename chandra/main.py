694afbeede0c241245bfe242c869d3b847f06060
Chandra Tritaqwa Ramadhan <lolpotch@gmail.com>
2024-12-18 15:33:17 +0700

    Init db before main

--- chandra/main.py
+++ chandra/main.py
@@ -1,7 +1,22 @@
-from login import login
+from database import engine, Base
+from database import session
+from login import Login, login
 from dashboard import Dashboard
 
+# Function to create tables
+def initialize_database():
+    print("Initializing database...")
+    Base.metadata.create_all(engine)  # Creates all tables
+    print("Database initialized successfully.")
+
+    # Run code below for first user initialization
+    # Comment them if you've add the user since it'd cause error if executed twice
+    # session.add(Login(username='admin', password='123'))
+    # session.commit()
+    # print("Admin user added successfully.")
+
 def main():
+    initialize_database() 
     while not login():
         pass
 
